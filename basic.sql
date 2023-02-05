5bcb402f33b49ae35be2fa040cc4e27c3ec2053f
Shrishti Tiwari <shrishtisreyansh@gmail.com>
2023-02-05 15:06:13 +0530

    basics

--- basic.sql
+++ basic.sql
@@ -1,15 +1,15 @@
 -- this will show the table-- 
 show tables;
 
--- this will describe the table-- 
+-- this will describe the table--
 desc sales;
 
--- This will select all the columns from your data-- 
-select * from sales;
+-- This will select all the columns from your data--
+SELECT * FROM sales;
 
 -- selecting a particular column from data (SaleDate, Amount, Customers)
 select SaleDate, Amount, Customers from sales;
 select Amount, Customers, GeoID from sales;
 
 -- Calculation--
-select SaleDate, Amount, Boxes, Amount/Boxes as 'Amount per Boxes' from sales; 
+select SaleDate, Amount, Boxes, Amount/Boxes as 'Amount per Boxes' from sales;
